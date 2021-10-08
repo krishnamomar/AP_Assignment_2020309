747bd0c22252de8ff149885fed3b13363519f0fe
Krishnam Omar <84073881+krishnamomar@users.noreply.github.com>
2021-10-08 20:28:45 +0530

    hospital and vaccine class added

---
+++ Assignment_1/src/com/aa_assignment/Hospital.java
@@ -0,0 +1,23 @@
+package com.aa_assignment;
+import java.util.*;
+import java.lang.*;
+
+public class Hospital {
+
+    String Name;
+    ArrayList<Vaccine> all_vaccine = new ArrayList<>();
+    ArrayList<Integer> v_available = new ArrayList<>();
+    ArrayList<Integer> day_number = new ArrayList<>();
+    Integer PinCode;
+    Integer Hosp_iD;
+
+    public void add_slot(Vaccine vx, Integer qnt, Integer day){
+        all_vaccine.add(vx);
+        v_available.add(qnt);
+        day_number.add(day);
+    }
+
+    public void remove(Integer x){
+        
+    }
+}
---
+++ Assignment_1/src/com/aa_assignment/Vaccine.java
@@ -0,0 +1,8 @@
+package com.aa_assignment;
+import java.lang.*;
+
+public class Vaccine {
+    String name;
+    int no_of_doses;
+    int gap_dose;
+}
