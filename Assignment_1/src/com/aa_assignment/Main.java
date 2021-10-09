1978bff3befa1ba98309953c9d8c7d71ae294db7
Krishnam Omar <84073881+krishnamomar@users.noreply.github.com>
2021-10-09 09:19:55 +0530

    main body

--- Assignment_1/src/com/aa_assignment/Main.java
+++ Assignment_1/src/com/aa_assignment/Main.java
@@ -4,9 +4,29 @@ import java.lang.*;
 import java.io.*;
 
 public class Main {
+    public static ArrayList<Patient> patient_list = new ArrayList<>();
+    public static ArrayList<Hospital> hospital_list = new ArrayList<>();
+    public static ArrayList<String> hospital_name = new ArrayList<>();
+    public static ArrayList<Integer> hospital_pin = new ArrayList<>();
+    public static ArrayList<Integer> hospital_ids = new ArrayList<>();
+    public static ArrayList<Vaccine> vaccine_list = new ArrayList<>();
+    public static ArrayList<String> vaccine_name = new ArrayList<>();
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException{
 	// write your code here
+        BufferedReader scn = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("CoVin Portal Initialized....");
+        System.out.println("===========================================");
+
+        while (true){
+
+        }
+
+
+
+        //scn.close(); call in the last
 
     }
 }
