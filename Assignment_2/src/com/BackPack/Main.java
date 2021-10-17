0495ef62170846208eba090b999ed78bc438cc6d
Krishnam Omar <84073881+krishnamomar@users.noreply.github.com>
2021-10-17 15:32:38 +0530

    Date Generation method

--- Assignment_2/src/com/BackPack/Main.java
+++ Assignment_2/src/com/BackPack/Main.java
@@ -1,8 +1,23 @@
 package com.BackPack;
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static String date_generator() {
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
+        String strDate = formatter.format(date);
+
+        return strDate;
+    }
+
+    public static void main(String[] args) throws IOException{
 	// write your code here
     }
 }
