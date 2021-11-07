7dd49829487542887bcb2c81e6ba0c7040fad12a
Krishnam Omar <84073881+krishnamomar@users.noreply.github.com>
2021-11-07 22:48:02 +0530

    triangular matrices checking

--- Assignment_3/src/com/Matrix/Main.java
+++ Assignment_3/src/com/Matrix/Main.java
@@ -1,6 +1,33 @@
 package com.Matrix;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public static boolean check_upper(ArrayList<ArrayList<Double>> grd){
+        for(int i=0; i<grd.size(); i++){
+            for (int j=0; j<grd.get(0).size(); j++){
+                if (i>j){
+                    if (grd.get(i).get(j)!=0){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean check_lower(ArrayList<ArrayList<Double>> grd){
+        for(int i=0; i<grd.size(); i++){
+            for (int j=0; j<grd.get(0).size(); j++){
+                if (i<j){
+                    if (grd.get(i).get(j)!=0){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
 	// write your code here
