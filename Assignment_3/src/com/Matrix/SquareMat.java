abf9b64a28d54e015c9437ba80a9b231429dfd80
Krishnam Omar <84073881+krishnamomar@users.noreply.github.com>
2021-11-07 13:40:49 +0530

    square matrix

---
+++ Assignment_3/src/com/Matrix/SquareMat.java
@@ -0,0 +1,36 @@
+package com.Matrix;
+
+import java.util.ArrayList;
+
+public class SquareMat extends matrix{
+    private String name;
+    private ArrayList<ArrayList<Integer>> grid;
+    private Integer row;
+    private Integer column;
+
+    public void setName(String nm){
+        this.name = nm;
+    }
+
+    public void setGrid(ArrayList<ArrayList<Integer>> grd){
+        this.grid = grd;
+        this.row = grd.size();
+        this.column = grd.size();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public ArrayList<ArrayList<Integer>> getGrid(){
+        return grid;
+    }
+
+    public Integer getRow(){
+        return row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+}
