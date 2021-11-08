31db0e2f22d7902890861a9fdf165a2bf872791c
Krishnam Omar <84073881+krishnamomar@users.noreply.github.com>
2021-11-08 14:29:01 +0530

    inversion

--- Assignment_3/src/com/Matrix/Main.java
+++ Assignment_3/src/com/Matrix/Main.java
@@ -86,8 +86,94 @@ public class Main {
         return ans;
     }
 
+    public static ArrayList<ArrayList<Double>> Inverse_Matrix(ArrayList<ArrayList<Double>> grd, Integer sz){
+        //only matrices with determinant != 0 can be inversed
+
+        if (sz==1){
+            ArrayList<ArrayList<Double>> ans = new ArrayList<>();
+            ArrayList<Double> x = new ArrayList<>();
+            Double v = Determinant(grd, sz);
+            v = 1/v;
+            x.add(v);
+            ans.add(x);
+
+            return ans;
+        }
+
+        else if (sz==2){
+            Double v = Determinant(grd, sz);
+            ArrayList<ArrayList<Double>> ans = new ArrayList<>();
+            for (int i=0; i<2; i++){
+                ArrayList<Double> x = new ArrayList<>();
+                ans.add(x);
+            }
+            ans.get(0).add((grd.get(1).get(1))/v);
+            ans.get(0).add(-1*((grd.get(0).get(1))/v));
+            ans.get(1).add(-1*((grd.get(1).get(0))/v));
+            ans.get(1).add((grd.get(0).get(0))/v);
+
+            return ans;
+
+        }
+
+        Double v = Determinant(grd, sz);
+        ArrayList<ArrayList<Double>> ans = new ArrayList<>();
+
+        for (int ii=0; ii<3; ii++){
+            ArrayList<Double> x = new ArrayList<>();
+            ans.add(x);
+        }
+
+        Double a = grd.get(0).get(0);
+        Double b = grd.get(0).get(1);
+        Double c = grd.get(0).get(2);
+        Double d = grd.get(1).get(0);
+        Double e = grd.get(1).get(1);
+        Double f = grd.get(1).get(2);
+        Double g = grd.get(2).get(0);
+        Double h = grd.get(2).get(1);
+        Double i = grd.get(2).get(2);
+
+        ans.get(0).add(((e*i)-(f*g))/v);
+        ans.get(0).add(((c*h)-(b*i))/v);
+        ans.get(0).add(((b*f)-(c*e))/v);
+        ans.get(1).add(((f*g)-(d*i))/v);
+        ans.get(1).add(((a*i)-(c*g))/v);
+        ans.get(1).add(((c*d)-(a*f))/v);
+        ans.get(2).add(((d*h)-(e*g))/v);
+        ans.get(2).add(((b*g)-(a*h))/v);
+        ans.get(2).add(((a*e)-(b*d))/v);
+
+        return ans;
+
+    }
+
     public static void main(String[] args) {
 	// write your code here
+
+        ArrayList<ArrayList<Double>> cntr = new ArrayList<>();
+        ArrayList<Double> x = new ArrayList<>();
+        ArrayList<Double> y = new ArrayList<>();
+        ArrayList<Double> z = new ArrayList<>();
+        x.add(1.0);
+        x.add(2.0);
+        x.add(2.0);
+        y.add(0.0);
+        y.add(5.0);
+        y.add(7.0);
+        z.add(1.0);
+        z.add(1.0);
+        z.add(1.0);
+        cntr.add(x);
+        cntr.add(y);
+        cntr.add(z);
+
+
+        print_matrix(cntr, 3, 3);
+        System.out.println(Determinant(cntr, 3));
+        ArrayList<ArrayList<Double>> cntr_inv = Inverse_Matrix(cntr, 3);
+        print_matrix(cntr_inv, 3, 3);
+
 
 
     }
