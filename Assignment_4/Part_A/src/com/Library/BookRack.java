f22f4dd3c9c3f74a553a49ef4ece602de1f8b29d
Krishnam Omar <84073881+krishnamomar@users.noreply.github.com>
2021-11-21 18:59:26 +0530

    debug little bit

--- Assignment_4/Part_A/src/com/Library/BookRack.java
+++ Assignment_4/Part_A/src/com/Library/BookRack.java
@@ -11,32 +11,21 @@ public class BookRack <T>{
     }
 
     public void Sort_(){
-
-        if (chk instanceof Book){
-            Book sortedrack[] = new Book[Racks.size()];
-            for (int i=0; i< Racks.size(); i++){
-                sortedrack[i] = (Book)Racks.get(i);
-            }
-
-            for (int i=0; i< Racks.size(); i++){
-                for (int j=i; j< Racks.size(); j++){
-                    if (sortedrack[i].isGreater(sortedrack[j])){
-                        Book x = sortedrack[j];
-                        sortedrack[j] = sortedrack[i];
-                        sortedrack[i] = x;
-                    }
+        Book sortedrack[] = new Book[Racks.size()];
+        for (int i=0; i< Racks.size(); i++){
+            sortedrack[i] = (Book)Racks.get(i);
+        }
+        for (int i=0; i< Racks.size(); i++){
+            for (int j=i; j< Racks.size(); j++){
+                if (sortedrack[i].isGreater(sortedrack[j])){
+                    Book x = sortedrack[j];
+                    sortedrack[j] = sortedrack[i];
+                    sortedrack[i] = x;
                 }
             }
-
-            for (int i=0; i< Racks.size(); i++){
-                this.Racks.set(i, (T)sortedrack[i]);
-            }
-
         }
-
-        else {
-            //this case never occurs XD
-            //Collections.sort(this.Racks);
+        for (int i=0; i< Racks.size(); i++){
+            this.Racks.set(i, (T)sortedrack[i]);
         }
     }
 
